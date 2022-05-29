d5b7a807bf7b9a8227b2ddef727b8fa241cec50f
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 16:12:32 +0600

    Connection with mongodb established successfully

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lmgp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
