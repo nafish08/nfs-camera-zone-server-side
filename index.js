89cf00967084e4aae7989c03897f3fee01691859
nafish08 <md.nafish.sadik@gmail.com>
2022-05-29 23:35:58 +0600

    readme file added

--- index.js
+++ index.js
@@ -12,12 +12,34 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lmgp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log(uri);
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('cameraZone').collection('product');
+        const reviewCollection = client.db('cameraZone').collection('review');
+
+        // For Products
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+        // For Reviews
+        app.get('/review', async (req, res) => {
+            const query = {};
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
